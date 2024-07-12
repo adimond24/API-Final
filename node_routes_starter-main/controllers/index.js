56cbe59aa85bff15e305fb2e7078b2653797a3e7
adimond24 <adimond24@s.tooeletech.edu>
2024-07-12 14:01:54 -0600

    finish

--- node_routes_starter-main/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (Req, res) => {
-  res.send("Hello World!");
-};
-
-const tooeleTech = (Req, res) => {
-  res.send("Tooele Tech is Awesome!");
-};
-
-module.exports = { awesomeFunction, tooeleTech };
--- node_routes_starter-main/routes/index.js
+++
@@ -1,8 +0,0 @@
-const myController = require("../controllers");
-const routes = require("express").Router();
-
-routes.get("/", myController.awesomeFunction);
-
-routes.get("/ttech", myController.tooeleTech);
-
-module.exports = routes;
